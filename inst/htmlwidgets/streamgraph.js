fde652a72cb640d5f8b3240fb38f3f33faf809e8
elfatherbrown <elfatherbrown@gmail.com>
2021-03-10 15:17:54 -0700

    Wrong types on the viewbox string

--- inst/htmlwidgets/streamgraph.js
+++ inst/htmlwidgets/streamgraph.js
@@ -127,7 +127,7 @@ HTMLWidgets.widget({
     .append("svg")
      /* .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom) */
-      .attr("viewBox","0 0 "+width + margin.left + margin.right+" "+height + margin.top + margin.bottom)
+      .attr("viewBox","0 0 "+(width + margin.left + margin.right)+" "+(height + margin.top + margin.bottom))
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
